f2815359c71ca2429e6fbb64677d9b0840b3b672
Luís Fonseca <luis@lhf.pt>
2023-01-13 14:03:39 +0000

    dark mode toggle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,54 @@
 <script lang="ts">
 	import 'uno.css';
+	import { browser } from '$app/environment';
+
+	let year = new Date().getFullYear();
+	let darkMode = true;
+
+	function handleToggleDarkMode() {
+		darkMode = !darkMode;
+
+		localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+	}
+
+	if (browser) {
+		let mediaPreference = window.matchMedia('(prefers-color-scheme: dark)').matches;
+		darkMode = localStorage.theme === 'dark' || (!('theme' in localStorage) && mediaPreference);
+	}
 </script>
 
-<slot />
+<div class={darkMode ? 'dark' : ''} text-center h-screen w-full>
+	<div
+		bg-white
+		dark:bg-black
+		text-black
+		dark:text-white
+		transition-colors
+		transition-duration-500
+		grid="~ rows-[min-content_min-content_1fr]"
+		h-full
+		px-8
+	>
+		<div pt-2 />
+		<div text-right text-gray4 py-2>
+			<button
+				on:click={handleToggleDarkMode}
+				text-3xl
+				dark:i-carbon-moon
+				i-carbon-sun
+				aria-label="Toggle dark mode"
+				title="Toggle dark mode"
+			/>
+		</div>
+		<slot />
+		<div text-center op-60 fw-200 py-2>
+			Copyright © {year} Luís Fonseca
+		</div>
+	</div>
+</div>
+
+<svelte:head>
+	<title>Luís Fonseca</title>
+	<meta name="description" content="Luís Fonseca's personal website" />
+	<meta name="author" content="Luís Fonseca" />
+</svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,11 @@
-<script lang="ts">
-	let year = new Date().getFullYear();
-</script>
-
-<div
-	flex
-	absolute
-	bottom-0
-	left-0
-	min-h-screen
-	min-w-screen
-	text-center
-	select-none
-	all:transition-400
->
-	<div ma>
-		<div text-5xl fw100 animate-bounce-alt animate-count-infinite animate-duration-2s>
-			Luís Fonseca
-		</div>
-		<div op30 text-lg fw300 m1>Come and read my procrastination.</div>
-		<div m2 flex justify-center text-2xl>
-			<a text-inherit href="https://github.com/luishfonseca">
-				<div i-carbon-logo-github op30 hover="op80" />
-			</a>
-		</div>
+<div ma>
+	<div text-5xl fw100 animate-bounce-alt animate-count-infinite animate-duration-2s>
+		Luís Fonseca
+	</div>
+	<div op-60 text-lg fw-300 m-1>Come and read my procrastination.</div>
+	<div m-2 flex justify-center text-2xl>
+		<a text-inherit href="https://github.com/luishfonseca" aria-label="Github">
+			<div i-carbon-logo-github op60 hover="op100" text-violet />
+		</a>
 	</div>
 </div>
-<div absolute bottom-5 right-0 left-0 text-center op30 fw300>Copyright © {year} Luís Fonseca</div>
