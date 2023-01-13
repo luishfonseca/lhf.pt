376b9aa1a667ac02e31e72b546aed9d2027f5e6d
Luís Fonseca <luis@lhf.pt>
2023-01-13 00:13:58 +0000

    basic website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let year = new Date().getFullYear();
+</script>
+
+<div
+	flex
+	absolute
+	bottom-0
+	left-0
+	min-h-screen
+	min-w-screen
+	text-center
+	select-none
+	all:transition-400
+>
+	<div ma>
+		<div text-5xl fw100 animate-bounce-alt animate-count-infinite animate-duration-2s>
+			Luís Fonseca
+		</div>
+		<div op30 text-lg fw300 m1>Come and read my procrastination.</div>
+		<div m2 flex justify-center text-2xl>
+			<a text-inherit href="https://github.com/luishfonseca">
+				<div i-carbon-logo-github op30 hover="op80" />
+			</a>
+		</div>
+	</div>
+</div>
+<div absolute bottom-5 right-0 left-0 text-center op30 fw300>Copyright © {year} Luís Fonseca</div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import UnoCSS from 'unocss/vite';
+import { extractorSvelte } from "@unocss/core";
+import { presetUno, presetAttributify, presetIcons } from 'unocss';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [
+		UnoCSS({
+			extractors: [extractorSvelte],
+			presets: [
+				presetUno(),
+				presetAttributify(),
+				presetIcons(),
+			],
+		}),
+		sveltekit(),
+	],
 };
 
 export default config;
