fe8f8f85fb0f4482f2f5bca4dd5569b297c948e7
Luís Fonseca <luis@lhf.pt>
2023-04-16 23:52:56 +0100

    deploy in london

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			regions: ['lhr1']
+		}),
 		prerender: {
 			crawl: true,
 			entries: ['/posts']
