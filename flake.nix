240f0afd8a5693e96baafcdb35b30c2d4e6bef30
Luís Fonseca <luis@lhf.pt>
2023-04-16 12:12:45 +0100

    use vercel for deployments

--- flake.nix
+++ flake.nix
@@ -14,23 +14,5 @@
       devShell = pkgs.mkShell {
         buildInputs = with pkgs; [ nodejs nodePackages.npm ];
       };
-
-      defaultPackage = pkgs.buildNpmPackage {
-        pname = "lhf-pt-website";
-        version = "0.0.1";
-
-        src = ./.;
-        npmDepsHash = "sha256-5Py0Qaam168qsqwFe/zVFXJxxHXJ33gTYzeRT0x5WBI=";
-
-        installPhase = ''
-          mkdir -p $out
-          npx ncc build build/ -m -o $out
-          cp -r build/client $out
-
-          # Make entrypoint executable
-          sed '1 i\#!${pkgs.nodejs}/bin/node' -i $out/index.js
-          chmod +x $out/index.js
-        '';
-      };
     });
 }
