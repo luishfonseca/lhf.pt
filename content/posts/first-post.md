560bf7200bd282c0a366e239d5410b6fcdc79dcc
Luís Fonseca <luis@lhf.pt>
2023-04-16 16:33:08 +0100

    format

--- content/posts/first-post.md
+++ content/posts/first-post.md
@@ -1,5 +1,5 @@
 ---
-title: "First Post"
+title: 'First Post'
 ---
 
 # this is the first post yay
--- src/routes/posts/[...path]/[slug]/+page.server.ts
+++ src/routes/posts/[...path]/[slug]/+page.server.ts
@@ -6,30 +6,33 @@ import * as matter from 'gray-matter';
 import { marked } from 'marked';
 
 marked.setOptions({
-    walkTokens: (token) => {
-        if (token.type === 'heading') {
-            token.depth += 1;
-        }
-    }
-})
+	walkTokens: (token) => {
+		if (token.type === 'heading') {
+			token.depth += 1;
+		}
+	}
+});
 
 const posts_dir = 'content/posts';
 
 export const load = (({ params }) => {
+	const file = `${posts_dir}/${params.path}/${params.slug}.md`;
 
-    const file = `${posts_dir}/${params.path}/${params.slug}.md`
+	// Sanitize the file path
+	if (
+		file.includes('\0') ||
+		!fs.existsSync(file) ||
+		!fs.realpathSync(file).startsWith(fs.realpathSync(posts_dir))
+	) {
+		throw error(404, 'Not found');
+	}
 
-    // Sanitize the file path
-    if (file.includes('\0') || !fs.existsSync(file) || !fs.realpathSync(file).startsWith(fs.realpathSync(posts_dir))) {
-        throw error(404, 'Not found')
-    }
+	const { data, content } = matter.read(file);
 
-    const { data, content } = matter.read(file);
-
-    return {
-        post: {
-            title: data.title,
-            content: marked(content)
-        }
-    };
+	return {
+		post: {
+			title: data.title,
+			content: marked(content)
+		}
+	};
 }) as PageServerLoad;
