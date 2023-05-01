35a69679a83a2089533bb68c05d8f238d9ff6f6c
Luís Fonseca <luis@lhf.pt>
2023-05-01 18:15:56 +0100

    change migrate script to typescript

---
+++ migrations/migrate.ts
@@ -0,0 +1,16 @@
+import { drizzle } from 'drizzle-orm/mysql2';
+import { migrate } from 'drizzle-orm/mysql2/migrator';
+import mysql from 'mysql2/promise';
+
+if (!process.env.DATABASE_URL) {
+	console.log("Missing DATABASE_URL environment variable")
+	process.exit(1)
+}
+
+const connection = await mysql.createConnection(process.env.DATABASE_URL);
+
+const db = drizzle(connection, { logger: true });
+
+migrate(db, { migrationsFolder: './migrations' }).finally(() => {
+	process.exit();
+})
