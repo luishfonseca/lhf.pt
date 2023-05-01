53ac03bce0e19f73ba47c109330c98095d8c3d9f
Luís Fonseca <luis@lhf.pt>
2023-05-01 18:50:34 +0100

    format

--- migrations/migrate.ts
+++ migrations/migrate.ts
@@ -3,8 +3,8 @@ import { migrate } from 'drizzle-orm/mysql2/migrator';
 import mysql from 'mysql2/promise';
 
 if (!process.env.DATABASE_URL) {
-	console.log("Missing DATABASE_URL environment variable")
-	process.exit(1)
+	console.log('Missing DATABASE_URL environment variable');
+	process.exit(1);
 }
 
 const connection = await mysql.createConnection(process.env.DATABASE_URL);
@@ -13,4 +13,4 @@ const db = drizzle(connection, { logger: true });
 
 migrate(db, { migrationsFolder: './migrations' }).finally(() => {
 	process.exit();
-})
+});
