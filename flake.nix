b0e133b8d7d1802d439b58a3198e5686cb180092
Luís Fonseca <luis@lhf.pt>
2023-07-01 23:34:51 +0100

    feat: add picocss

--- flake.nix
+++ flake.nix
@@ -4,15 +4,82 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.05";
     utils.url = "github:numtide/flake-utils";
+
+    picocss = {
+      url = "github:picocss/pico/v2.0.0-alpha1";
+      flake = false;
+    };
   };
 
   outputs = { ... } @ inputs: inputs.utils.lib.eachDefaultSystem (system:
     let
       pkgs = import inputs.nixpkgs { inherit system; };
+
+      zola = pkgs.zola.overrideAttrs (prev: rec {
+        src = pkgs.fetchFromGitHub {
+          owner = "luishfonseca";
+          repo = "zola";
+          rev = "master";
+          sha256 = "sha256-9qt30cxFVNVa8IpkGO/Q196oPk/GpjKT245JpxKxqSQ=";
+        };
+
+        cargoDeps = prev.cargoDeps.overrideAttrs (pkgs.lib.const {
+          inherit src;
+          outputHash = "sha256-RkZr0zaFMyphYAj+dmIXKYyHFXnUIAaikReh8RQeW1w=";
+        });
+      });
+
+      vercelCfg = builtins.toJSON {
+        version = 3;
+      };
+
+      web = pkgs.stdenv.mkDerivation {
+        name = "lhf.pt-web";
+
+        src = ./web;
+
+        patchPhase = ''
+          cp -r ${inputs.picocss}/scss sass/_pico
+        '';
+
+        installPhase = ''
+          mkdir $out
+
+          cat > $out/config.json << EOF
+          ${vercelCfg}
+          EOF
+
+          ${zola}/bin/zola build -o $out/static
+        '';
+      };
+
+      deploy = { prod, ci ? true }: pkgs.writeScript "deploy" ''
+        ${pkgs.nodePackages.vercel}/bin/vercel pull --yes --environment ${if prod then "production" else "development"} ${if ci then "--token=$VERCEL_TOKEN" else ""}
+
+        rm -rf .vercel/output
+        mkdir -p .vercel/output
+        cp -r ${web}/* .vercel/output
+        chmod -R +w .vercel/output
+
+        ${pkgs.nodePackages.vercel}/bin/vercel deploy --prebuilt ${if prod then "--prod" else ""} ${if ci then "--token=$VERCEL_TOKEN" else ""}
+      '';
+
+      mkApp = run: { type = "app"; program = "${run}"; };
     in
     {
       devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [ zola ];
+        shellHook = ''
+          unlink web/sass/_pico
+          ln -s ${inputs.picocss}/scss web/sass/_pico
+        '';
+
+        buildInputs = [ zola pkgs.nodePackages.vercel ];
       };
+
+      defaultPackage = web;
+
+      apps.deploy-prod = mkApp (deploy { prod = true; });
+      apps.deploy-dev = mkApp (deploy { prod = true; });
+      apps.deploy-not-ci = mkApp (deploy { prod = false; ci = false; });
     });
 }
