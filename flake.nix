314e39f5e674af24e22ba43260b7da7ecb78907f
Luís Fonseca <luis@lhf.pt>
2024-06-19 13:12:39 +0100

    chore: cleanup flake

--- flake.nix
+++ flake.nix
@@ -11,67 +11,97 @@
     };
   };
 
-  outputs = { ... } @ inputs: inputs.utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import inputs.nixpkgs { inherit system; };
+  outputs = {...} @ inputs:
+    inputs.utils.lib.eachDefaultSystem (system: let
+      pkgs = import inputs.nixpkgs {inherit system;};
 
       vercelCfg = builtins.toJSON {
         version = 3;
         routes = [
-          { handle = "filesystem"; }
-          { src = "/(.*)"; status = 404; dest = "/404.html"; }
+          {handle = "filesystem";}
+          {
+            src = "/(.*)";
+            status = 404;
+            dest = "/404.html";
+          }
         ];
       };
 
-      web = { prod }: pkgs.stdenv.mkDerivation {
-        name = "lhf.pt-web";
+      web = {prod}:
+        pkgs.stdenv.mkDerivation {
+          name = "lhf.pt-web";
+          src = ./web;
+          patchPhase = ''
+            cp -r ${inputs.picocss}/scss sass/_pico
+          '';
 
-        src = ./web;
+          installPhase = ''
+            mkdir $out
 
-        patchPhase = ''
-          cp -r ${inputs.picocss}/scss sass/_pico
-        '';
+            cat > $out/config.json << EOF
+            ${vercelCfg}
+            EOF
 
-        installPhase = ''
-          mkdir $out
+            ${pkgs.zola}/bin/zola build -o $out/static ${
+              if prod
+              then ""
+              else "-u=https://preview.lhf.pt"
+            }
+          '';
+        };
 
-          cat > $out/config.json << EOF
-          ${vercelCfg}
-          EOF
+      deploy = {
+        prod,
+        ci ? true,
+      }: let
+        app = pkgs.writeScriptBin "deploy" (''
+            #!${pkgs.stdenv.shell}
 
-          ${pkgs.zola}/bin/zola build -o $out/static ${if prod then "" else "-u=https://preview.lhf.pt"}
-        '';
+            ${pkgs.nodePackages.vercel}/bin/vercel pull --yes --environment ${
+              if prod
+              then "production"
+              else "development"
+            } ${pkgs.lib.optionalString ci "--token=$VERCEL_TOKEN"}
+
+            rm -rf .vercel/output
+            mkdir -p .vercel/output
+            cp -r ${web {inherit prod;}}/* .vercel/output
+            chmod -R +w .vercel/output
+
+            url=$(${pkgs.nodePackages.vercel}/bin/vercel deploy --prebuilt ${
+              if prod
+              then "--prod"
+              else ""
+            } ${pkgs.lib.optionalString ci "--token=$VERCEL_TOKEN"})
+          ''
+          + (
+            pkgs.lib.optionalString (! prod) ''
+              ${pkgs.nodePackages.vercel}/bin/vercel alias --yes $url preview.lhf.pt  ${pkgs.lib.optionalString ci "--token=$VERCEL_TOKEN"}
+            ''
+          ));
+      in {
+        type = "app";
+        program = "${app}/bin/deploy";
       };
-
-      deploy = { prod, ci ? true }: pkgs.writeScript "deploy" (''
-        ${pkgs.nodePackages.vercel}/bin/vercel pull --yes --environment ${if prod then "production" else "development"} ${if ci then "--token=$VERCEL_TOKEN" else ""}
-
-        rm -rf .vercel/output
-        mkdir -p .vercel/output
-        cp -r ${web { inherit prod; }}/* .vercel/output
-        chmod -R +w .vercel/output
-
-        url=$(${pkgs.nodePackages.vercel}/bin/vercel deploy --prebuilt ${if prod then "--prod" else ""} ${if ci then "--token=$VERCEL_TOKEN" else ""})
-      '' + (if prod then "" else ''
-        ${pkgs.nodePackages.vercel}/bin/vercel alias --yes $url preview.lhf.pt ${if ci then "--token=$VERCEL_TOKEN" else ""}
-      ''));
-
-      mkApp = run: { type = "app"; program = "${run}"; };
-    in
-    {
+    in {
       devShell = pkgs.mkShell {
         shellHook = ''
           unlink web/sass/_pico
           ln -s ${inputs.picocss}/scss web/sass/_pico
         '';
 
-        buildInputs = with pkgs; [ zola nodePackages.vercel ];
+        buildInputs = with pkgs; [zola nodePackages.vercel];
       };
 
-      defaultPackage = web { prod = true; };
+      defaultPackage = web {prod = true;};
 
-      apps.deploy-prod = mkApp (deploy { prod = true; });
-      apps.deploy-dev = mkApp (deploy { prod = false; });
-      apps.deploy-not-ci = mkApp (deploy { prod = false; ci = false; });
+      apps.deploy-prod = deploy {prod = true;};
+      apps.deploy-dev = deploy {prod = false;};
+      apps.deploy-not-ci = deploy {
+        prod = false;
+        ci = false;
+      };
+
+      formatter = pkgs.alejandra;
     });
 }
